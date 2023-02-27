884019214b50196661fa39e93fa1e4fac9d5ee18
Luis Cuellar <luisenri2901@gmail.com>
2023-02-26 21:56:07 -0500

    Para crer una nueva cuenta, se hace el llamado al objeto cuenta, creando también una variable del mismo objeto y así editar o asignar un valor al atributo seleccionado

--- src/Crear_Cuenta.java
+++ src/Crear_Cuenta.java
@@ -2,6 +2,8 @@
 public class Crear_Cuenta {
 	public static void main(String[] args) {
 		Cuenta primeraCuenta = new Cuenta(); //Crea una nueva Cuenta, la variable es tipo Cuenta
+		primeraCuenta.saldo = 1000;
 		
+		System.out.println(primeraCuenta.saldo);
 	}
 }
