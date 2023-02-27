ebaa67bc423f8757c2d07e1fa0b05a2bab32520f
Luis Cuellar <luisenri2901@gmail.com>
2023-02-26 21:52:31 -0500

    Empezando a explorar la orientación a objetos en java. Se plantea la creación/simulación de un banco y las cuentas bancarias

---
+++ src/Crear_Cuenta.java
@@ -0,0 +1,7 @@
+
+public class Crear_Cuenta {
+	public static void main(String[] args) {
+		Cuenta primeraCuenta = new Cuenta(); //Crea una nueva Cuenta, la variable es tipo Cuenta
+		
+	}
+}
---
+++ src/Cuenta.java
@@ -0,0 +1,15 @@
+/*Entidad Cuenta: Class para representar entidades
+ * Saldo
+ * Agencia
+ * Numero
+ * Titular 
+ */
+
+class Cuenta {
+	
+	double saldo;
+	int agencia;
+	int numero;
+	String titular;
+
+}
