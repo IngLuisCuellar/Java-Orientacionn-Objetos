30554d1dec331c4294c34f282990c5b84968df06
Luis Cuellar <luisenri2901@gmail.com>
2023-02-27 22:25:52 -0500

    Se realia el último método, transferir. Para este se creó otro objeto cuenta y se utilizó el método ya conocido de depositar, (También se puede usar el de retirar). Se optimizó el código, haciendolo más corto y quitando obviedades

--- src/Cuenta.java
+++ src/Cuenta.java
@@ -14,7 +14,7 @@ class Cuenta {
 	
 	//Void --> No retorna valor
 	public void depositar(double saldo){//El dinero que se va agregar a la cuenta
-		this.saldo = this.saldo + saldo;
+		this.saldo += saldo;
 		// This.saldo es el saldo de este objeto (Cuenta), por otro lado, saldo es el parámetro
 		//que le ingresa
 	}
@@ -23,12 +23,19 @@ class Cuenta {
 	// Retorna true or false
 	public boolean retirar(double saldo){ //Boolean porque retorna si es un exito o un error
 		if(this.saldo >= saldo) {
-			this.saldo = this.saldo - saldo;
+			this.saldo -= saldo;
 			return true;
-		} else {
-			System.out.println("No es posible retirar");
-			return false;
 		}
+		return false;
+	}
+								//Objeto tipo cuenta
+	public boolean transferir(double saldo, Cuenta cuenta) {
+		if (this.saldo >= saldo) {
+			this.saldo -= saldo;
+			cuenta.depositar(saldo);
+			return true;
+		}
+		return false;
 	}
 
 }
--- src/pruebaMetodos.java
+++ src/pruebaMetodos.java
@@ -2,12 +2,21 @@
 public class pruebaMetodos {
 	public static void main(String[] args) {
 		Cuenta miCuenta = new Cuenta();
+		Cuenta segundaCuenta = new Cuenta();
 		miCuenta.saldo = 300;
 		miCuenta.depositar(1500);
 		
 		System.out.println(miCuenta.saldo);
 		
 		miCuenta.retirar(300);
+		System.out.println(miCuenta.saldo);
+		
+		///
+		
+		segundaCuenta.depositar(1000);
+		segundaCuenta.transferir(600,miCuenta);
+		
+		System.out.println(segundaCuenta.saldo);
 		System.out.println(miCuenta.saldo);
 	}
 }
